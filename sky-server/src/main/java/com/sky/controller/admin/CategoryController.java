cad83f39664f42e91f1ad95923d67ccd6403c031
lishebly <lishebly@gmail.com>
2024-02-06 16:43:45 +0800

    分类模块初始化以及修改功能开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -0,0 +1,31 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.CategoryDTO;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import com.sky.service.EmployeeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/category")
+@Slf4j
+@Api(tags = "分类相关接口")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @ApiOperation("修改分类")
+    @PutMapping
+    public Result updateCategory(@RequestBody CategoryDTO categoryDTO){
+        log.info("修改分类{}",categoryDTO);
+        categoryService.updateCategory(categoryDTO);
+        return Result.success();
+    }
+}
